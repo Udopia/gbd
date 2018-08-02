b59c5583a9549c2c152a09538d6d2cd42a0569a2
Markus Iser <2.markus.iser@gmail.com>
2018-08-02 16:19:13 +0200

    cleanups and data

--- search.py
+++ search.py
@@ -6,37 +6,23 @@ import re
 from tatsu import parse
 from tatsu.util import asjson
 
-def find_hashes(database, cat):
-  return database.value_query("SELECT hash FROM {}".format(cat))
+def find_hashes(database, query=None):
+  if query is None:
+    return database.value_query("SELECT hash FROM benchmarks")
+  else:
+    ast = parse(GRAMMAR, query)
+    where = build_where(ast)
+    tables = build_join(ast)
+    tables.discard("benchmarks")
+    joined = "benchmarks "
+    for table in tables:
+      joined += "left join " + table + " on benchmarks.hash = " + table + ".hash "
+    return database.value_query("SELECT benchmarks.hash FROM {} WHERE {}".format(joined, where))
 
-def find_hash(database, cat, tags):
-  is_numeric = groups.reflect_type(database, cat).lower() != 'text'
+def resolve(database, cat, hash):
+  return database.value_query("SELECT value FROM {} WHERE hash='{}'".format(cat, hash))
 
-  regex = r"^([<>=]?=?)(\-?[\.0-9]+)$" if is_numeric else r"^([<>=]=?)?([^ \t\n\r\f\v]*)$"
-
-  constraints = []
-  for tag in tags:
-    match = re.search(regex, tag)
-    if match is None:
-      raise ValueError("Argument {} does not match pattern '{}', which is required for group {}".format(tag, regex, cat))
-
-    default_operator = "=" if is_numeric else "LIKE"
-    operator = match.group(1) or default_operator
-
-    prefix = '"' if match.group(1) is not None else '"%'
-    postfix = '"' if match.group(1) is not None else '%"'
-    value = match.group(2) if is_numeric else prefix + match.group(2) + postfix
-
-    constraints.append("value {} {}".format(operator, value))
-
-  where = ' AND '.join(["{}"]*len(constraints)).format(*constraints)
-  sql = "SELECT hash FROM {} WHERE {}".format(cat, where)
-  result = database.value_query(sql)
-
-  return result
-
-
-GRAMMAR = '''
+GRAMMAR = r'''
     @@grammar::EXP
     @@ignorecase::True
 
@@ -84,16 +70,3 @@ def build_join(ast):
   elif ast["op"] is not None:
     result.add(ast["attr"])
   return result
-
-def find_hashes_by_query(database, query):
-  ast = parse(GRAMMAR, query)
-  where = build_where(ast)
-  tables = build_join(ast)
-  tables.discard("benchmarks")
-  joined = "benchmarks "
-  for table in tables:
-    joined += "left join " + table + " on benchmarks.hash = " + table + ".hash "
-  return database.value_query("SELECT benchmarks.hash FROM {} WHERE {}".format(joined, where))
-
-def resolve(database, cat, hash):
-  return database.value_query("SELECT value FROM {} WHERE hash='{}'".format(cat, hash))
