53f4c6cdc548483e0dcb3bed49887aa3186d26e8
Markus Iser <2.markus.iser@gmail.com>
2018-08-22 19:38:31 +0200

    wrote data import for csv-data with type-inference; some smaller refinements

---
+++ import_csv.py
@@ -0,0 +1,56 @@
+import csv
+import re
+import groups
+import tags
+
+def exists(database, cat):
+  g = groups.reflect(database)
+  return (cat in g)
+
+def determine_type(database, values):
+  is_real = False
+  re_int = re.compile('[0-9]+')
+  re_double = re.compile('[0-9]+\.[0-9]+')
+  for value in values:
+    value = value.strip()
+    if not value:
+      continue
+    if re_int.fullmatch(value) is None:
+      is_real = True
+      if re_double.fullmatch(value) is None:
+        return "text"
+  return "integer" if not is_real else "real"
+
+def get_header(csv_file, key_column="instance"):
+  fieldnames = []
+  with open(csv_file, newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='\'')
+    fieldnames = [field for field in csvreader.fieldnames if field != key_column]
+  return fieldnames
+
+def create_group(database, csv_file, db_column, csv_column):
+  with open(csv_file, newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='\'')
+    values = [line[csv_column] for line in csvreader]
+    sqltype = determine_type(database, values)
+    print('Column {} has type {} [values: {}, {}, {}, ...]'.format(csv_column, sqltype, values[0], values[1], values[2]))
+    groups.add(database, db_column, unique=False, type=sqltype, default=None)
+
+def import_csv(database, csv_file, column_prefix="imported_", key_column="instance"):
+  # create group
+  csv_columns = get_header(csv_file, key_column)
+  for csv_column in csv_columns:
+    db_column = "{}{}".format(column_prefix, csv_column)
+    if not exists(database, db_column):
+      pat = re.compile(r"^[a-zA-Z][a-zA-Z0-9_]*$")
+      if not pat.match(db_column):
+        raise ValueError("Column name {} does not match regular expression".format(db_column))
+      create_group(database, csv_file, db_column, csv_column)
+    with open(csv_file, newline='') as csvfile:
+      csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='\'')
+      for row in csvreader:
+        if row[csv_column].strip():
+          instance = row[key_column]
+          value = row[csv_column]
+          print("Inserting into table {}: instance {} value {}".format(db_column, instance, value))
+          tags.add_tag(database, db_column, instance, value)
