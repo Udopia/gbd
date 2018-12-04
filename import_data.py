040078e632b75c8b8a11e5d8871252cc6d27fb33
Markus Iser <2.markus.iser@gmail.com>
2018-12-04 15:05:14 +0100

    refined data-import

--- import_data.py
+++ import_data.py
@@ -21,32 +21,33 @@ def determine_type(database, values):
         return "text"
   return "integer" if not is_real else "real"
 
-def get_header(csv_file, key_column):
+def get_header(filename, key_column):
   fieldnames = []
-  with open(csv_file, newline='') as csvfile:
+  with open(filename, newline='') as csvfile:
     csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='\'')
     fieldnames = [field for field in csvreader.fieldnames if field != key_column]
   return fieldnames
 
-def create_group(database, csv_file, db_column, csv_column):
-  with open(csv_file, newline='') as csvfile:
-    csvreader = csv.DictReader(csvfile, delimiter=',', quotechar='\'')
+def create_group(database, filename, csv_column, db_column):
+  with open(filename, newline='') as csvfile:
+    csvreader = csv.DictReader(csvfile, delimiter=' ', quotechar='\'')
     values = [line[csv_column] for line in csvreader]
     sqltype = determine_type(database, values)
     print('Column {} has type {} [values: {}, {}, {}, ...]'.format(csv_column, sqltype, values[0], values[1], values[2]))
     groups.add(database, db_column, unique=True, type=sqltype, default=None)
 
-def import_csv(database, csv_file, key_column, column_names, column_prefix=""):
-  for column in column_names:
-    db_column = "{}{}".format(column_prefix, column)
-    if not exists(database, db_column):
-      pat = re.compile(r"^[a-zA-Z][a-zA-Z0-9_]*$")
-      if not pat.match(db_column):
-        raise ValueError("Column name {} does not match regular expression".format(db_column))
-      create_group(database, csv_file, db_column, column)
-    with open(csv_file, newline='') as csvfile:
-      print("Inserting into table {} ... ".format(db_column))
+def import_csv(database, filename, key, source, target):
+  with open(filename, newline='') as csvfile:
+    if not exists(database, target):
+      print("Creating table {}".format(target))
+      create_group(database, csvfile, source, target)
       csvreader = csv.DictReader(csvfile, delimiter=' ', quotechar='\'')
-      lst = [(row[key_column], row[column]) for row in csvreader if row[column].strip()]
-      print("Inserting {} values into table {}".format(len(lst), db_column))
-      database.bulk_insert(db_column, lst)
+      lst = [(row[key], row[source]) for row in csvreader if row[source].strip()]
+      print("Inserting {} values into table {}".format(len(lst), target))
+      database.bulk_insert(target, lst)
+    else:
+      csvreader = csv.DictReader(csvfile, delimiter=' ', quotechar='\'')
+      lst = [(row[key], row[source]) for row in csvreader if row[source].strip()]
+      print("Attempting to insert {} values into table {}".format(len(lst), target))
+      for (hash_, value_) in lst:
+        tags.add_tag(database, target, value_, hash_, False)
