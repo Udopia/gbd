e62562713a089f0fd9b0bf84a062b3ee5de60206
Luca <luca_springer@yahoo.com>
2019-01-11 18:28:40 +0100

    Implemented query function in RestAPI

---
+++ server.py
@@ -0,0 +1,47 @@
+from tatsu import exceptions
+from core.main import groups, search
+from core.database.db import Database
+
+from flask import Flask, render_template, request
+
+app = Flask(__name__)
+
+from os.path import realpath, dirname, join
+
+DATABASE = join(dirname(realpath(__file__)), 'local.db')
+
+
+@app.route("/", methods={'GET'})
+def welcome():
+    return render_template('home.html')  # file is found at runtime
+
+
+@app.route("/resolve/<hash>", methods=['GET'])
+def resolve(hash):
+    param = request.args.to_dict()
+    result = ""
+    with Database(DATABASE) as database:
+        if len(param.keys()) == 0:
+            attributes = groups.reflect(database)
+
+        for name in param.keys():
+            value = search.resolve(database, name, hash)
+            result += "{} {}\n".format(name, *value)
+    return result
+
+
+@app.route("/query", methods=['POST'])   # query string über post
+def query():
+    response = ""
+    dict = request.values.to_dict()
+    query = dict["query"]
+    with Database(DATABASE) as database:
+        try:
+            list = search.find_hashes(database, query)
+        except exceptions.FailedParse:
+            return "Not a valid query argument"
+        for hash in list:
+            path = search.resolve(database, "benchmarks", hash)
+            response += "<div>{} on path {}</div>\n".format(hash, path)
+    return response
+    #TODO: form
