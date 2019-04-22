938280dbdef79c875f0e16b32ef0cfa9870cd709
Luca <luca_springer@yahoo.com>
2019-04-22 23:30:41 +0200

    Adjust queryzip method to new return type of resolve

--- zipper.py
+++ zipper.py
@@ -1,14 +1,15 @@
 import os
-import server
 from os.path import basename
 from zipfile import ZipFile
+
+import server
 
 
 def create_zip_with_marker(zipfile, files, prefix):
     server.ZIP_SEMAPHORE.acquire()
     with ZipFile(zipfile, 'w') as zf:
         for file in files:
-            zf.write(*file, basename(*file))
+            zf.write(file, basename(file))
     zf.close()
     os.rename(zipfile, zipfile.replace(prefix, ''))
     server.ZIP_SEMAPHORE.release()
