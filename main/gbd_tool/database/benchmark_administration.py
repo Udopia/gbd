1af4c5499023c3def5ed2b84ab89c9d6aa3b79d5
Markus Iser <2.markus.iser@gmail.com>
2019-05-17 12:52:07 +0200

    fix bug in db initialization

--- main/gbd_tool/database/benchmark_administration.py
+++ main/gbd_tool/database/benchmark_administration.py
@@ -6,7 +6,6 @@ from main.gbd_tool.database.db import Database, DatabaseException
 from main.gbd_tool.hashing.gbd_hash import gbd_hash
 from main.util.util import eprint
 
-
 def add_tag(database, cat, tag, hash, force=False):
     info = groups.reflect(database, cat)
     if (info[0]['unique']):
@@ -45,32 +44,30 @@ def add_benchmark(database, hash, path):
             database.submit('INSERT OR IGNORE INTO {} (hash) VALUES ("{}")'.format(group, hash))
 
 
-def remove_benchmarks(db):
-    with Database(db) as database:
-        paths = database.value_query("SELECT value FROM benchmarks")
-        for p in paths:
-            if not isfile(p):
-                eprint("Problem '{}' not found. Removing...".format(p))
-                database.submit("DELETE FROM benchmarks WHERE value='{}'".format(p))
+def remove_benchmarks(database):
+    paths = database.value_query("SELECT value FROM benchmarks")
+    for p in paths:
+        if not isfile(p):
+            eprint("Problem '{}' not found. Removing...".format(p))
+            database.submit("DELETE FROM benchmarks WHERE value='{}'".format(p))
 
-
-def register_benchmarks(db, benchmark_root):
+# todo: parallelize hashing
+def register_benchmarks(database, benchmark_root):
     for root, dirnames, filenames in os.walk(benchmark_root):
         for filename in filenames:
-            path = os.path.join(root, filename)
-            eprint('Found {}'.format(path))
-            try:
-                with Database(db) as database:
+            if filename.endswith(".cnf") or filename.endswith(".cnf.gz") or filename.endswith(".cnf.lzma") or filename.endswith(".cnf.bz2"):
+                path = os.path.join(root, filename)
+                eprint('Found {}'.format(path))
+                try:
                     hashes = database.value_query("SELECT hash FROM benchmarks WHERE value = '{}'".format(path))
                     if len(hashes) is not 0:
                         eprint('Problem {} already hashed'.format(path))
                         continue
-                hashvalue = gbd_hash(path)
-                with Database(db) as database:
+                    hashvalue = gbd_hash(path)
                     add_benchmark(database, hashvalue, path)
-            except DatabaseException as e:
-                eprint(e)
-                return
-            except UnicodeDecodeError as e:
-                eprint('Skipping file due to decoding error: {}'.format(e))
-                continue
+                except DatabaseException as e:
+                    eprint(e)
+                    return
+                except UnicodeDecodeError as e:
+                    eprint('Skipping file due to decoding error: {}'.format(e))
+                    continue
