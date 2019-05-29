4314fde77eaee2bac62d0b116492fa36e030ce54
Luca <luca_springer@yahoo.com>
2019-05-29 17:30:37 +0200

    Initialize gbd_api with path to db

--- main/gbd_tool/gbd_api.py
+++ main/gbd_tool/gbd_api.py
@@ -12,36 +12,46 @@ from main.gbd_tool.hashing.gbd_hash import gbd_hash
 from main.gbd_tool.http_client import post_request
 
 
+# create a new gbd_api object which operates on a database. The file for this database is parameterized in the
+# constructor and cannot be changed
+def __init__(self, database=None):
+    if database is not None:
+        if self.database is None:
+            self.database = database
+    else:
+        raise ValueError("Tried to create api_object without a path to database")
+
+
 # hash a CSV file
 def hash_file(path):
     return gbd_hash(path)
 
 
 # Import CSV file
-def import_file(database, path, key, source, target):
-    with Database(database) as database:
+def import_file(self, path, key, source, target):
+    with Database(self.database) as database:
         import_data.import_csv(database, path, key, source, target)
 
 
 # Initialize the GBD database. Create benchmark entries in database if path is given, just create a database otherwise.
 # With the constructor of a database object the __init__ method in db.py will be called
-def init_database(db, path=None):
+def init_database(self, path=None):
     if path is not None:
-        benchmark_administration.remove_benchmarks(db)
-        benchmark_administration.register_benchmarks(db, path)
+        benchmark_administration.remove_benchmarks(self.database)
+        benchmark_administration.register_benchmarks(self.database, path)
     else:
-        Database(db)
+        Database(self.database)
 
 
 # Get information of the whole database
-def get_database_info(database):
-    with Database(database) as database:
+def get_database_info(self):
+    with Database(self.database) as database:
         return {'name': database, 'version': database.get_version(), 'hash-version': database.get_hash_version()}
 
 
 # Checks weather a group exists in given database object
-def check_group_exists(database, name):
-    with Database(database) as database:
+def check_group_exists(self, name):
+    with Database(self.database) as database:
         if name in groups.reflect(database):
             return True
         else:
@@ -49,33 +59,33 @@ def check_group_exists(database, name):
 
 
 # Adds a group to given database representing for example an attribute of a benchmark
-def add_attribute_group(database, name, type, unique):
-    with Database(database) as database:
+def add_attribute_group(self, name, type, unique):
+    with Database(self.database) as database:
         groups.add(database, name, unique is not None, type, unique)
 
 
 # Remove group from database
-def remove_attribute_group(database, name):
-    with Database(database) as database:
+def remove_attribute_group(self, name):
+    with Database(self.database) as database:
         groups.remove(database, name)
 
 
 # Get all groups which are in the database
-def get_all_groups(database):
-    with Database(database) as database:
+def get_all_groups(self):
+    with Database(self.database) as database:
         return groups.reflect(database)
 
 
 # Delete entries in groups from database, but don't delete the according group
-def clear_group(database, name):
-    with Database(database) as database:
+def clear_group(self, name):
+    with Database(self.database) as database:
         groups.remove(database, name)
 
 
 # Retrieve information about a specific group
-def get_group_info(database, name):
+def get_group_info(self, name):
     if name is not None:
-        with Database(database) as database:
+        with Database(self.database) as database:
             return {'name': name, 'type': groups.reflect_type(database, name),
                     'uniqueness': groups.reflect_unique(database, name),
                     'default': groups.reflect_default(database, name),
@@ -85,23 +95,23 @@ def get_group_info(database, name):
 
 
 # Retrieve all values the given group contains
-def get_group_values(database, name):
+def get_group_values(self, name):
     if name is not None:
-        return query_search(database, '{} like %%%%'.format(name))
+        return query_search(self.database, '{} like %%%%'.format(name))
     else:
         raise ValueError('No group given')
 
 
 # Associate hashes with a hash-value in a group
-def set_attribute(database, name, value, hash_list, force):
-    with Database(database) as database:
+def set_attribute(self, name, value, hash_list, force):
+    with Database(self.database) as database:
         for h in hash_list:
             benchmark_administration.add_tag(database, name, value, h, force)
 
 
 # Remove association of a hash with a hash-value in a group
-def remove_attribute(database, name, value, hash_list):
-    with Database(database) as database:
+def remove_attribute(self, name, value, hash_list):
+    with Database(self.database) as database:
         for h in hash_list:
             benchmark_administration.remove_tag(database, name, value, h)
 
@@ -118,8 +128,8 @@ def hash_intersection(hash_list, other_hash_list):
 
 # Search for benchmarks which have to pertain to the query semantics. Before searching, some groups must be added and
 # filled with hashes and their values for the according group. Returns list of hashes
-def query_search(database, query=None):
-    with Database(database) as database:
+def query_search(self, query=None):
+    with Database(self.database) as database:
         try:
             hashes = search.find_hashes(database, query)
         except sqlite3.OperationalError:
@@ -137,8 +147,8 @@ def query_request(host, query, useragent):
 
 # Resolve hashes against groups. Returns a list of dictionaries (one for each hash) and several entries
 # (their values in given groups)
-def resolve(database, hash_list, group_list, pattern=None, collapse=False):
-    with Database(database) as database:
+def resolve(self, hash_list, group_list, pattern=None, collapse=False):
+    with Database(self.database) as database:
         result = []
         for h in hash_list:
             out = {'hash': h}
