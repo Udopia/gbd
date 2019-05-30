0afa04695d5bb06f731519a140e845803a99a214
Luca <luca_springer@yahoo.com>
2019-05-30 15:15:02 +0200

    Improve config_manager and gbd_api - allow custom config - check config file - raise Error if file not exists or lacks keys - hierarchical order: First custom config, then standard config, if no custom is given - different config classes operate on the same config directory of gbd - static methods in gbd_api

--- main/gbd_tool/config_manager.py
+++ main/gbd_tool/config_manager.py
@@ -5,31 +5,58 @@ from flask import json
 
 sys_dir = '/etc'
 gbd_dir = join(sys_dir, 'gbd')
-config_file = 'conf.json'
+standard_config_file = 'conf.json'
+custom_config_file = 'custom_conf.json'
 db_file = 'local.db'
 db_key = 'database'
+custom_exists = False
 
 
 class ConfigManager:
-    def __init__(self, database_path=None):
-        if database_path is not None:
-            if self.database_path is None:
-                self.database_path = database_path
+    def __init__(self, json_file=None):
+        if json_file is not None:
+            self.make_custom_configuration(json_file)
+            global custom_exists
+            custom_exists = True
         else:
             self.database_path = db_file
+        self.config_keys = [db_key]
 
-    def make_standard_configuration(self):
+    @staticmethod
+    def make_standard_configuration():
         if not os.path.isdir(gbd_dir):
             os.mkdir(gbd_dir)
         if not isfile(join(gbd_dir, db_file)):
             open(join(gbd_dir, db_file), 'w').close()
-        open(join(gbd_dir, config_file), 'w').close()
-        f = open(join(gbd_dir, config_file), 'w')
+        open(join(gbd_dir, standard_config_file), 'w').close()
+        f = open(join(gbd_dir, standard_config_file), 'w')
         f.write('{}\n'.format(json.dumps({db_key: join(gbd_dir, db_file)})))
         f.close()
 
-    def get_config_file_path(self):
-        return join(gbd_dir, config_file)
+    # Absolute path for custom config file is needed
+    def make_custom_configuration(self, json_file):
+        if not os.path.isdir(gbd_dir):
+            os.mkdir(gbd_dir)
+        if isfile(json_file):
+            if self.check_config(json_file):
+                custom = open(json_file, 'r')
+                content = custom.read()
+                custom.close()
+
+                config = open(join(gbd_dir, custom_config_file), 'w')
+                config.write(content)
+                config.close()
+            else:
+                raise ValueError('Given json config file lacks keys')
+        else:
+            raise ValueError('Given config file not found')
+
+    @staticmethod
+    def get_config_file_path():
+        if custom_exists:
+            return join(gbd_dir, custom_config_file)
+        else:
+            return join(gbd_dir, standard_config_file)
 
     def get_config_dictionary(self):
         f = open(self.get_config_file_path(), 'r')
@@ -38,3 +65,15 @@ class ConfigManager:
     def get_database_path(self):
         config_dict = self.get_config_dictionary()
         return config_dict.get(db_key)
+
+    def check_config(self, config_file):
+        f = open(config_file, 'r')
+        dictionary = json.loads(f.read())
+        try:
+            for key in self.config_keys:
+                dictionary.get(key)
+        except KeyError:
+            f.close()
+            return False
+        f.close()
+        return True
