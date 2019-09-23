ccaadc7a3a686bc39549c713f938d2b2703907e8
Luca Springer <luca_springer@yahoo.com>
2019-09-23 23:22:04 +0200

    Adjust setup.py for pypi

--- main/setup.py
+++ main/setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup
 
 setup(name='global_benchmark_database_tool',
-      version='2.2.5',
+      version='2.2.6',
       description='A tool for global benchmark management',
       long_description=open('README.md', 'rt').read(),
       long_description_content_type="text/markdown",
