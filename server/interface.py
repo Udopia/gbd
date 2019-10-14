a1937a8bfd1651b51ed40def359801f926fe2863
Markus Iser <2.markus.iser@gmail.com>
2019-10-14 14:24:54 +0200

    added gpl header to all py files, changed license to gpl

--- server/interface.py
+++ server/interface.py
@@ -1,2 +1,18 @@
+# Global Benchmark Database (GBD)
+# Copyright (C) 2019 Markus Iser, Luca Springer, Karlsruhe Institute of Technology (KIT)
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
 from os.path import join, dirname, realpath
 SERVER_CONFIG_PATH = join(dirname(realpath(__file__)), "server_config")
