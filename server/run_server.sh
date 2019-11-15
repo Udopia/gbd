11067c4597281b76856d8e85b3b05eed6c87d0ce
Luca Springer <luca_springer@yahoo.com>
2019-11-15 18:15:13 +0100

    Modified server script to echo used database

--- server/run_server.sh
+++ server/run_server.sh
@@ -4,8 +4,10 @@ if [ $# -lt 1 ]; then
     echo "Usage: $0 [database] or set environment variable GBD_DB"
     exit 0
   else
+    echo "Using fallback: ${GBD_DB}"
     python3 server.py -d ${GBD_DB}
   fi
 else
+    echo "Using database: $1"
   python3 server.py -d $1
 fi
