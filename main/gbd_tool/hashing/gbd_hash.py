058b73ccdbfcf3148676c96d9ee99de989bfaf97
Markus Iser <markus.iser@kit.edu>
2020-01-03 17:51:46 +0100

    optimized hash fkt

--- main/gbd_tool/hashing/gbd_hash.py
+++ main/gbd_tool/hashing/gbd_hash.py
@@ -16,12 +16,12 @@
 
 import bz2
 import gzip
+import lzma
 import hashlib
 import io
-import lzma
-import mmap
 import re
 import sys
+import time
 
 from gbd_tool.util import eprint
 
@@ -32,11 +32,12 @@ __all__ = ['gbd_hash', 'HASH_VERSION']
 # Hash-Version 2: fixed bug in version 1 (do not skip -)
 # Hash-Version 3: add trailing zero to last clause if missing
 
-HASH_VERSION = 3
+HASH_VERSION = 2
 
 class CNFNormalizerVersion:
     def __init__(self, filename):
         self.space = False
+        self.skip = False
         self.start = True
         if filename.endswith('.cnf.gz'):
             self.f = gzip.open(filename, 'rb')
@@ -48,41 +49,83 @@ class CNFNormalizerVersion:
             self.f = open(filename, 'rb')
         else:
             raise Exception("Unknown CNF file-type")
-        self.m = mmap.mmap(self.f.fileno(), 0, access=mmap.ACCESS_READ)
 
     def __enter__(self):
         return self
 
     def read(self):
-        eprint("this is a test")
         buf = bytearray()
-        byte = b' '
-        while len(buf) < 65536 and byte != b'':
-            byte = self.m.read(1)
-            if byte <= b' ':
-                self.space = not self.start  # merge whitespaces
-            elif byte == b'c' or byte == b'p':  # exclude comments and header
-                while byte != b'\n' or byte != b'\r' or byte != b'':
-                    byte = self.m.read(1)
-            elif byte >= b'0' and byte <= b'9' or byte == b'-':
-                if self.space:
-                    buf.append(ord(b' '))  # pending whitespace
+        while len(buf) < 65536:
+            byte = self.f.read(1)
+            if byte == b'':
+                return buf  # end of file
+            if not self.skip and (byte >= b'0' and byte <= b'9' or byte == b'-'):
+                if self.space and not self.start:
+                    buf.append(ord(b' '))  # append pending space
                     self.space = False
                 buf.append(ord(byte))
                 self.start = False
+            elif byte == b'c' or byte == b'p':
+                self.skip = True  # do not hash comment and header line
+            elif byte <= b' ':
+                self.space = True  # do not immediately append spaces but remember that there was at least one
+                if byte == b'\n' or byte == b'\r':
+                    self.skip = False  # comment line ended
         return buf
 
     def __exit__(self, exc_type, exc_value, traceback):
         self.f.close()
 
 
-def gbd_hash(fname):
+def gbd_hash_old(fname):
+    start = time.time()
     hash_md5 = hashlib.md5()
     with CNFNormalizerVersion(fname) as f:
         for chunk in iter(lambda: f.read(), b''):
             hash_md5.update(chunk)
+    hash_md5.update(b'\n')
+    end = time.time()
+    print(end - start)
     return hash_md5.hexdigest()
 
+
+def gbd_hash(fname):
+    Tstart = time.time()
+    space = False
+    skip = False
+    start = True
+    if fname.endswith('.cnf.gz'):
+        f = gzip.open(fname, 'rb')
+    elif fname.endswith('.cnf.bz2'):
+        f = bz2.open(fname, 'rb')
+    elif fname.endswith('.cnf.lzma'):
+        f = lzma.open(fname, 'rb')
+    elif fname.endswith('.cnf'):
+        f = open(fname, 'rb')
+    else:
+        raise Exception("Unknown CNF file-type")
+    hash_md5 = hashlib.md5()
+
+    for byte in iter(lambda: f.read(1), b''):
+        if not skip and (byte >= b'0' and byte <= b'9' or byte == b'-'):
+            if space and not start:
+                hash_md5.update(b' ')  # append pending space
+                space = False
+            hash_md5.update(byte)
+            start = False
+        elif byte == b'c' or byte == b'p':
+            skip = True  # do not hash comment and header line
+        elif byte <= b' ':
+            space = True  # do not immediately append spaces but remember that there was at least one
+            if byte == b'\n' or byte == b'\r':
+                skip = False  # comment line ended
+
+    hash_md5.update(b'\n')
+    f.close()
+    
+    Tend = time.time()
+    eprint("Seconds to hash: {:1.2}".format(Tend - Tstart))
+    return hash_md5.hexdigest()
 
 def hash_hashlist(hashlist):
     hash_md5 = hashlib.md5()
