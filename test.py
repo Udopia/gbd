9451e816e18a53656ee0b280c87845cc4d2b5cca
Markus Iser <markus.iser@kit.edu>
2020-01-09 13:41:09 +0100

    gbd hash tests

--- test.py
+++ test.py
@@ -1,27 +1,37 @@
 import unittest
 import io
 
-from gbd_tool.hashing.gbd_hash import gbd_hash_inner as gbd_hash
+from main.gbd_tool.hashing.gbd_hash import gbd_hash_inner as gbd_hash
 
 class TestGBDHash(unittest.TestCase):
 
-    def __init__(self):
-        self.normalized = b"1 2 0 3 0"
-        self.with_header = b"p cnf 2 2\n1 2 0 3 0"
-        self.linebreaks = b"p cnf 2 2\n1 2 0\r\n 3 0"
-        self.linebreaks2 = b"\np cnf 2 2\n1 2 0\r\n 3 0\n"
-        self.comments = b"c test\np cnf 2 2\n1 2 0\r\n 3 0\n"
-        self.spaces = b" c test\np cnf 2 2 \n1 2 0     \r\n 3 0 \n "
-        self.missing0 = b"p cnf 2 2\n1 2 0\n3"
+    def setUp(self):
+        self.reference = gbd_hash(io.BytesIO(b"1 2 0 3 0"))
 
-    def test_gbd_hash(self):
-        reference = gbd_hash(io.BytesIO(self.normalized))
-        self.assertEqual(reference, io.BytesIO(self.with_header))
-        self.assertEqual(reference, io.BytesIO(self.linebreaks))
-        self.assertEqual(reference, io.BytesIO(self.linebreaks2))
-        self.assertEqual(reference, io.BytesIO(self.comments))
-        self.assertEqual(reference, io.BytesIO(self.spaces))
-        self.assertEqual(reference, io.BytesIO(self.missing0))  
+    def tearDown(self):
+        pass
+    
+    def test_normalize_header(self):
+        variant = gbd_hash(io.BytesIO(b"p cnf 2 2\n1 2 0 3 0"))
+        self.assertEqual(self.reference, variant)
+
+    def test_normalize_linebreaks(self):
+        variant1 = gbd_hash(io.BytesIO(b"p cnf 2 2\n1 2 0\r\n 3 0"))
+        variant2 = gbd_hash(io.BytesIO(b"\np cnf 2 2\n1 2 0\r\n 3 0\n"))
+        self.assertEqual(self.reference, variant1)
+        self.assertEqual(self.reference, variant2)
+
+    def test_normalize_comments(self):
+        variant = gbd_hash(io.BytesIO(b"c test\np cnf 2 2\n1 2 0\r\n 3 0\n"))
+        self.assertEqual(self.reference, variant)
+
+    def test_normalize_spaces(self):
+        variant = gbd_hash(io.BytesIO(b" c test\np cnf 2 2 \n1 2 0     \r\n 3 0 \n "))
+        self.assertEqual(self.reference, variant)
+
+    def test_normalize_missing_trailing_zero(self):
+        variant = gbd_hash(io.BytesIO(b"p cnf 2 2\n1 2 0\n3"))
+        self.assertEqual(self.reference, variant)
 
 
 if __name__ == '__main__':
