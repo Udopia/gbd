0073baf8597804a5c21fa2242de9b3d4be7a1766
Markus Iser <markus.iser@kit.edu>
2020-02-13 17:20:56 +0100

    update script

--- update_tool.sh
+++ update_tool.sh
@@ -1,5 +1,3 @@
-sudo python3 setup.py develop
-sudo python3 setup.py sdist
-sudo python3 setup.py  bdist_wheel
+sudo python3 setup.py develop sdist bdist_wheel
 twine upload dist/*
 sudo rm -Rf global_benchmark_database_tool.egg-info dist build
