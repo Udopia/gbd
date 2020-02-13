e90df2234abd38488c16c5da02e34e872969aba5
Markus Iser <markus.iser@kit.edu>
2020-02-13 15:26:09 +0100

    added entry points

--- setup.py
+++ setup.py
@@ -1,25 +1,28 @@
-from setuptools import setup
+from setuptools import setup, find_packages
 
 setup(name='global_benchmark_database_tool',
-      version='2.4.2',
-      description='A tool for global benchmark management',
-      long_description=open('README.md', 'rt').read(),
-      long_description_content_type="text/markdown",
-      url='https://github.com/Udopia/gbd',
-      author='Markus Iser, Luca Springer',
-      author_email='',
-      license='MIT',
-      classifiers=[
-          "License :: OSI Approved :: MIT License",
-          "Programming Language :: Python :: 3",
-          "Programming Language :: Python :: 3.7",
-      ],
-      packages=['gbd_tool', 'gbd_tool/database',
-                'gbd_tool/hashing'],
-      include_package_data=True,
-      install_requires=[
-          'flask',
-          'setuptools',
-          'tatsu',
-      ],
-      zip_safe=False)
+  version='2.4.3',
+  description='Maintenance of Benchmark Instances and their Attributes',
+  long_description=open('README.md', 'rt').read(),
+  long_description_content_type="text/markdown",
+  url='https://github.com/Udopia/gbd',
+  author='Markus Iser, Luca Springer',
+  author_email='gbd@informatik.kit.edu',
+  packages=['gbd_tool', 'gbd_tool/database',
+            'gbd_tool/hashing'],
+  include_package_data=True,
+  install_requires=[
+      'flask',
+      'setuptools',
+      'tatsu',
+  ],
+  classifiers=[
+    "License :: OSI Approved :: GNU General Public License v3 (GPLv3)",
+    "Programming Language :: Python :: 3"
+  ],
+  entry_points={
+    "console_scripts": [
+        "gbd = cli:main"
+    ]
+  }
+)
