a9dd7e5da952e2a43bf85445ff0d8fc37dbff832
Markus Iser <markus.iser@kit.edu>
2020-02-13 17:12:28 +0100

    include gbd.py in setup

--- setup.py
+++ setup.py
@@ -1,14 +1,15 @@
 from setuptools import setup, find_packages
 
 setup(name='global_benchmark_database_tool',
-  version='2.4.6',
+  version='2.4.7',
   description='Maintenance of Benchmark Instances and their Attributes',
   long_description=open('README.md', 'rt').read(),
   long_description_content_type="text/markdown",
   url='https://github.com/Udopia/gbd',
   author='Markus Iser, Luca Springer',
   author_email='gbd@informatik.kit.edu',
-  packages=find_packages('.'),
+  packages=find_packages(),
+  scripts=["gbd.py"],
   include_package_data=True,
   install_requires=[
       'flask',
