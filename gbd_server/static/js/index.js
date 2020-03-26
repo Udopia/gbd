2f50bc58550008390d23e2cb91499a34fcf41f6d
Luca Springer <uhyzv@student.kit.edu>
2020-03-26 16:34:46 +0100

    Implement bootstrap vue form and result table - loading while busy - reset form - pagination of table

--- gbd_server/static/js/index.js
+++ gbd_server/static/js/index.js
@@ -1,58 +1,81 @@
 var app = new Vue({
-        el: '#app',
-        data: {
+    el: '#app',
+    data: {
+        form: {
             query: '',
             groups: [],
             selected_groups: [],
         },
-        methods: {
-            getHost: function() {
-                var protocol = location.protocol;
-                var slashes = protocol.concat("//");
-                var port = location.port;
-                return slashes.concat(window.location.hostname).concat(':').concat(port);
-            },
-            getGroups: function() {
-                $.ajax({
-                    url: this.getHost().concat("/getgroups"),
-                    type: 'GET',
-                    dataType: 'json',
-                    success: function (result) {
-                        for (let object in result) {
-                            app.groups.push({'name': result[object]});
-                        }
-                    },
-                    error: function (error) {
-                        console.log('Error: '.concat(error.toString()));
-                        alert('Something went wrong. Check the console for details.');
-                    }
-                })
-            },
-            submitQuery: function(event) {
-                var jsonData = {
-                        query: this.query,
-                        selected_groups: this.selected_groups,
-                    };
-                $.ajax({
-                    url: this.getHost().concat("/results"),
-                    type: 'POST',
-                    data: JSON.stringify(jsonData),
-                    contentType: 'application/json; charset=utf-8',
-                    dataType: 'json',
-                    success: function (result) {
-                        alert('Got result') // TODO: Handle result
-                    },
-                    error: function (error) {
-                        console.log('Error: '.concat(error.toString()));
-                        alert('Something went wrong. Check the console for details.');
-                    }
-                });
-                event.preventDefault();
-            },
+        show_form: true,
+        result: [],
+        table_busy: false,
+        current_page: 1,
+        per_page: 5,
+    },
+    methods: {
+        getHost: function () {
+            var protocol = location.protocol;
+            var slashes = protocol.concat("//");
+            var port = location.port;
+            return slashes.concat(window.location.hostname).concat(':').concat(port);
         },
-        mounted: function () {
-                  this.$nextTick(function () {
-                    this.getGroups();
-                  })
-            },
-    });
+        getGroups: function () {
+            $.ajax({
+                url: this.getHost().concat("/getgroups"),
+                type: 'GET',
+                dataType: 'json',
+                success: function (result) {
+                    for (let object in result) {
+                        app.form.groups.push({'text': result[object], 'value': result[object]});
+                    }
+                },
+                error: function (error) {
+                    console.log('Error: '.concat(error.toString()));
+                    alert('Something went wrong. Check the console for details.');
+                }
+            })
+        },
+        submitQuery: function (event) {
+            app.table_busy = true;
+            var jsonData = {
+                query: this.form.query,
+                selected_groups: this.form.selected_groups,
+            };
+            $.ajax({
+                url: this.getHost().concat("/results"),
+                type: 'POST',
+                data: JSON.stringify(jsonData),
+                contentType: 'application/json; charset=utf-8',
+                dataType: 'json',
+                success: function (result) {
+                    app.result = result;
+                    app.table_busy = false;
+                },
+                error: function (error) {
+                    console.log('Error: '.concat(error.toString()));
+                    alert('Something went wrong. Check the console for details.');
+                }
+            });
+            event.preventDefault();
+        },
+        onReset: function (event) {
+            event.preventDefault();
+            this.form.query = '';
+            this.form.selected_groups = [];
+            this.show = false;
+            this.$nextTick(() => {
+                this.show = true;
+            });
+        }
+    },
+    mounted: function () {
+        this.$nextTick(function () {
+            this.getGroups();
+        })
+    },
+    computed: {
+        rows() {
+            return this.result.length
+        }
+    }
+});
