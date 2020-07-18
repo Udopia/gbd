59559da513190805166a52abc9efbb3bb9e1cc15
Markus Iser <markus.iser@kit.edu>
2020-07-18 15:57:23 +0200

    gbd-hash.sh

--- gbd_hash.sh
+++ gbd_hash.sh
@@ -9,6 +9,10 @@ command="cat"
 
 if [[ $file == *.gz ]]; then 
   command="zcat"
+elif [[ $file == *.bz2 ]]; then
+  command="bzcat"
+elif [[ $file == *.xz ]]; then
+  command="xzcat"
 fi
 
 $command $file | grep -v '^c\|^p' | tr '\r\n' ' ' | sed 's/\s\+/ /g;s/^\s//g;s/\s$//g' | sed 's/[^0]$/& 0/' | md5sum
