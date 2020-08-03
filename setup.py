c26e0c5fe7df05423c8fc13cf9623391357b3714
Markus Iser <markus.iser@kit.edu>
2020-08-03 13:15:39 +0200

    version bump

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(name='gbd_tools',
-  version='3.0.4',
+  version='3.0.5',
   description='GBD Benchmark Database Tools: Maintenance of Benchmark Instances and their Attributes',
   long_description=open('README.md', 'rt').read(),
   long_description_content_type="text/markdown",
