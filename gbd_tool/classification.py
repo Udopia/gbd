992fbf2ded292766e5f69ac977030a0f3ee04644
Elizaveta Danilova <uoujv@student.kit.edu>
2022-02-03 09:12:19 +0100

    Classification Shorter dictionary preparation and integration into classify2 method.

--- gbd_tool/classification.py
+++ gbd_tool/classification.py
@@ -30,7 +30,7 @@ def set_up_dictionnaries(df):
 
     for col in df.columns:
 
-        dict_str_c[col] = {}
+        #dict_str_c[col] = {}
         dict_str_e[col] = {}
         dict_flt_c[col] = {}
         dict_flt_f[col] = {}
@@ -147,14 +147,61 @@ def classify(api: GBD, query, feature, hashes, features, collapse,group_by, time
 
 def classify2(api: GBD, query, feature, hashes, features, collapse,group_by, timeout_memout, filenames, replace_dict):
 
+
+
     resultset = api.query_search2(query, feature, hashes, features, collapse, group_by,
                                   timeout_memout, replace_dict, filenames)
 
-    (df, dict_str_c) = set_up_dictionnaries(resultset)
+
+    #Creates dictionaries for the translation of non-numerical and conituous numerical entries
+    dict_str_e = {}
+    dict_flt_f ={}
+
+
+    for col in resultset.columns:
+
+        dict_str_e[col] = {}
+        dict_flt_f[col] = {}
+        s_str = set()
+        s_flt = set()
+
+        #Fills two set with values that should be replaced
+        for i in range(len(resultset)):
+            e = resultset.iloc[i][col]
+
+            if util.is_number(e):
+                if not float(e).is_integer():
+                    s_flt.add(float(e))
+            else:
+                s_str.add(e)
+
+        #Fills the dictionaries with a translation of the set-values and a counter
+        counter_str = -1
+        for e in s_str:
+            dict_str_e[col][e] = counter_str
+            counter_str = counter_str - 1
+
+        counter_flt = -1
+        for f in s_flt:
+            dict_flt_f[col][f] = counter_flt
+            counter_flt = counter_flt-1
+
+        #Replaces the undesired values by the integer using the dictionaries
+        for i in range(len(resultset)):
+            e = resultset.iloc[i][col]
+            if e in dict_str_e[col]:
+                resultset.at[i, col] = dict_str_e[col][e]
+            elif float(e) in dict_flt_f[col]:
+                resultset.at[i, col] = dict_flt_f[col][float(e)]
+
+    #Converts dataframe to the int-type.
+    df = resultset.astype(np.int)
+
+
 
     #split train and test data
-    x = resultset
-    y = resultset.pop(feature)
+    x = df
+    y = df.pop(feature)
 
     x_train, x_test, y_train, y_test = train_test_split(x, y, test_size = 0.2, random_state = 0)
 
@@ -166,18 +213,11 @@ def classify2(api: GBD, query, feature, hashes, features, collapse,group_by, tim
 
     # store classificator and dictionary
     if (filenames != []):
-        with open(filenames[0] + '_dict.txt', 'w') as convert_file:
-            convert_file.write(json.dumps(dict_str_c))
         piskle.dump(clf, filenames[0] + '.pskl')
 
     if(len(filenames) == 3):
         # read classificator and dictionary
         clf2 = piskle.load(filenames[1])
-        dict_str_c = {}
-        with open(filenames[1] + '_dict.txt') as f:
-            for line in f:
-                (key, val) = line.split()
-                dict_str_c[int(key)] = val
 
 
         cl = clf2.predict(x_test)
@@ -189,85 +229,12 @@ def classify2(api: GBD, query, feature, hashes, features, collapse,group_by, tim
         print(classification_report(y_test, class_df))
 
         # stores the result in case a filename is given
-        if (filenames[2] != 'empty'):
-            pd.set_option('display.max_rows', None)
-            file = open(filenames[2], 'w')
-            #file.writelines("Score: "+ str(s)+".\n")
-            file.writelines("Classification report: "+ classification_report(y_test, class_df)+".\n")
-            file.close()
-
-
-
-    
-'''def classify_train(api: GBD, feature, resultset, features, filename):
-
-    (df, dict_str_c) = set_up_dictionnaries(resultset)
-
-    #Prepares the training data
-    x = df.copy()
-    y = pd.DataFrame(x.pop(feature), columns=[feature])
-
-    #Builds the classificator on the training data
-    clf = tree.DecisionTreeClassifier()
-    clf = clf.fit(x.to_numpy(), y.to_numpy())
-
-    # store classificator and dictionary
-    with open(filename+'_dict.txt', 'w') as convert_file:
-        convert_file.write(json.dumps(dict_str_c))
-    piskle.dump(clf, filename+'.pskl')'''
-
-
-'''def classify_test(api: GBD, feature, df, features, filename):
-
-    # read classificator and dictionary
-    clf = piskle.load(filename)
-    dict_str_c = {}
-    with open(filename+'_dict.txt') as f:
-        for line in f:
-            (key, val) = line.split()
-            dict_str_c[int(key)] = val
-
-    #Prepares the test data
-    test_classes = df.pop(feature)
-    test_classes = test_classes.reset_index(drop=True)
-
-    #Applies the classificator on the test data
-    cl = clf.predict(df)
-
-    class_df = pd.DataFrame(cl)
-    class_df.columns = ['predicted']
-
-    #Creates a dataframe with the comparison of the actual and the predicted features with the re-translation
-    diff = pd.concat([test_classes, class_df], axis=1, join="inner")
-
-    diff = diff.astype(str)
-
-    for col in diff.columns:
-        for i in range(len(diff)):
-            c = str(diff.at[i, col])
-            if c in str(dict_str_c[feature]):
-                diff.at[i, col] = str(dict_str_c[feature][int(c)])
-            else:
-                diff.at[i, col] = "invalid"
-
-    print(diff)
-
-    #Counts how often the data was predicted wrongly
-    misses = 0
-
-    for i in range(len(diff)):
-        if diff.at[i, feature] != diff.at[i, 'predicted']:
-            misses = misses + 1
-
-    print("The classifier has a total of "+str(misses)+" misses.")
-    print("This makes up a total of "+ str(misses / len(diff))+ " percent fault rate.")'''
-
-'''#stores the result in case a filename is given
-    if(filename != 'empty'):
+        #if (filenames[2] != 'empty'):
         pd.set_option('display.max_rows', None)
-        file = open(filename, 'w')
-        print(diff, file=file)
-        file.writelines("The classifier has a total of "+str(misses)+" misses.\n")
-        file.write("This makes up a total of "+ str(misses / len(diff))+ " percent fault rate.")
-        file.close()'''
+        file = open(filenames[2], 'w')
+        #file.writelines("Score: "+ str(s)+".\n")
+        file.writelines("Classification report: "+ classification_report(y_test, class_df)+".\n")
+        file.close()
+
+
 
