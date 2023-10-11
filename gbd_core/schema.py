da5cf427c4b9583eeb36d432b2daab3af71de2e6
Markus Iser <markus.iser@kit.edu>
2023-10-11 13:57:08 +0200

    api-function for dbname-from-path

--- gbd_core/schema.py
+++ gbd_core/schema.py
@@ -145,7 +145,7 @@ class Schema:
 
 
     @classmethod
-    def dbname_from_path(cls, path):        
+    def dbname_from_path(cls, path):
         filename = os.path.basename(path)
         if filename[0].isdigit():
             filename = "cnf_" + filename
