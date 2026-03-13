ba098fe021189445dc6141aeb9dd118463d8cee7
Ashlin Iser <iser@kit.edu>
2026-03-13 10:51:05 +0100

    modernized release script and info

--- update_tool.sh
+++ update_tool.sh
@@ -1,7 +1,4 @@
-sudo rm -rf dist/
-# sudo python3 setup.py develop sdist bdist_wheel
-# twine upload dist/*
-sudo python3 -m pip install --upgrade build twine
-sudo python3 -m build
+rm -rf dist/
+# sudo python3 -m pip install --upgrade build twine
+python3 -m build
 twine upload dist/*
-sudo rm -Rf gbd_tools.egg-info dist build
